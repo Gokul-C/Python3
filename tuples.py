83e1fad867a31e8d1caf46591a2e62ba69811c9a
Gokul-C <gokulc741@gmail.com>
2021-07-28 10:31:50 +0530

    second commit-added description

--- tuples.py
+++ tuples.py
@@ -1,13 +1,13 @@
 #tuples = collection which are orderd and unchangeable..
 #          used to group together related data.
 
-student = ("hasi",22,"female")
+student = ("neeli",22,"female")   #computers always starts with zero,so its 0,1,2
 
-print(student.index("female"))
-print(student.count("neelima"))
+print(student.index("female"))    #index() means its in 2 place in student tuple. 
+print(student.count("neeli"))     #count() , its i 1st place in student tuple.
 
 for i in student:
     print (i)
-
-if "hasi" in student:
+    
+if "neeli" in student:
     print("gokul@kali_linux")   
